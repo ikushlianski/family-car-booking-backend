63e53ce7a4ce72a15c4d097d122be63ecccfb01c
Ilya Kushlianski <kushliansky@gmail.com>
2022-08-01 15:39:11 +0300

    Add dev and qa scripts to package.json

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,13 +1,14 @@
-import { MyStack } from "./MyStack";
-import { App } from "@serverless-stack/resources";
+import { MyStack } from './MyStack';
+import { App } from '@serverless-stack/resources';
 
 export default function (app: App) {
   app.setDefaultFunctionProps({
-    runtime: "nodejs16.x",
-    srcPath: "services",
+    runtime: 'nodejs16.x',
+    srcPath: 'services',
     bundle: {
-      format: "esm",
+      format: 'esm',
     },
   });
+
   app.stack(MyStack);
 }
