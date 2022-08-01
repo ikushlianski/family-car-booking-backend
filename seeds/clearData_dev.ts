2d36f84b22137260689a3b49200131b83c8608bd
Ilya Kushlianski <kushliansky@gmail.com>
2022-08-01 14:38:15 +0300

    Move seed generation files

---
+++ seeds/clearData_dev.ts
@@ -0,0 +1,21 @@
+import {FAMILY_HONDA_CAR_NUMBER} from 'services/core/car/car.constants';
+import {HondaTrackerDynamoService} from 'services/db/db.service';
+
+(async () => {
+  await Promise.all([
+    // remove users
+    HondaTrackerDynamoService.entities.user
+      .delete({ username: 'papa' })
+      .go(),
+    HondaTrackerDynamoService.entities.user
+      .delete({ username: 'ilya' })
+      .go(),
+
+    // remove cars
+    HondaTrackerDynamoService.entities.car
+      .delete({ carId: FAMILY_HONDA_CAR_NUMBER })
+      .go(),
+
+    // todo remove bookings
+  ]);
+})();
---
+++ seeds/defaultData_dev.ts
@@ -0,0 +1,46 @@
+import { FAMILY_HONDA_CAR_NUMBER } from 'services/core/car/car.constants';
+import { UserRoles } from 'services/core/user/user.constants';
+import { HondaTrackerDynamoService } from 'services/db/db.service';
+
+(async () => {
+  // cars
+  const insertCarHonda = HondaTrackerDynamoService.entities.car
+    .create({
+      carId: FAMILY_HONDA_CAR_NUMBER,
+      username: 'owner#papa',
+    })
+    .go();
+
+  // users
+  const insertUserIlya = HondaTrackerDynamoService.entities.user
+    .create({
+      username: 'ilya',
+      password: process.env.ILYA_PASSWORD as string,
+      roles: [UserRoles.DRIVER],
+      availableCarIds: [FAMILY_HONDA_CAR_NUMBER],
+      rideCompletionText: 'Машина в гараже',
+      notifications: {
+        getNotifiedWhenBookingChanged: true,
+        getNotifiedWhenBookingCreated: true,
+      },
+    })
+    .go();
+
+  const insertUserPapa = HondaTrackerDynamoService.entities.user
+    .create({
+      username: 'papa',
+      password: process.env.PAPA_PASSWORD as string,
+      roles: [UserRoles.CAR_PROVIDER, UserRoles.DRIVER],
+      availableCarIds: [FAMILY_HONDA_CAR_NUMBER],
+      rideCompletionText: 'Машина в гараже',
+      notifications: {
+        getNotifiedWhenBookingChanged: true,
+        getNotifiedWhenBookingCreated: true,
+      },
+    })
+    .go();
+
+  await Promise.all([insertCarHonda, insertUserPapa, insertUserIlya]);
+
+  console.log('done');
+})();
