02149fc75b75fc1bff0ccb0c9397048fd76a65be
Ilya Kushlianski <kushliansky@gmail.com>
2022-08-08 16:53:43 +0300

    When running all integration tests, exit on error in any test suite

--- specs/run-integration.sh
+++ specs/run-integration.sh
@@ -10,6 +10,12 @@ if [ "$1" != "" ]; then
 else
   for file in $(find ./specs/features -type f -name "*.feature" -exec basename {} \;); do
      jest "$file"
+
+     exit_status=$?
+
+     if [ $exit_status -gt 0 ]; then
+       exit $exit_status
+     fi
   done
 
 fi
